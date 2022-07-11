94619dca67505a400acf89771c804e22bcad8b77
Jose A. De la hoz <josehoz.ing@gmail.com>
2022-07-11 12:27:46 -0500

    Codigo manejo de cadenas

--- Codigos/Cadenas.py
+++ Codigos/Cadenas.py
@@ -1,5 +1,5 @@
 def run():
-    nombre = input('Ingrese su nombre: ')
+    nombre = input('Ingrese nombre: ')
     saludo = f'Mucho gusto {nombre}, bienvenido al mundo de python.'
     print(saludo)
     print(f'El mensaje anterior tiene {len(saludo)} caracteres')
