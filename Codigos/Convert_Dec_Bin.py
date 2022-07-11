81df6fa6e03986784b815edc8eb23ef5639600dd
Jose A. De la hoz <josehoz.ing@gmail.com>
2022-07-11 18:23:11 -0500

    Codigo conversor de decimal a binario

---
+++ Codigos/Convert_Dec_Bin.py
@@ -0,0 +1,24 @@
+
+def Convert_Decimal_Binary(numero):
+    """
+    Ingresa un número entero y retorna una lista con
+    el número equivalente en binario
+    """
+    binario = []
+    while(numero >= 1):
+        if numero%2==0: 
+            binario.append(0) #Agergar a la lista un '0' si el numero es módulo 2
+            numero//=2
+        elif numero%2!=0: #Agregar un '1' a la lista si el numero no es módulo de 2
+            numero//=2
+            binario.append(1)
+    return(binario[::-1])
+
+
+def run():
+    numero = int(input('Ingrese un numero entero: '))
+    binario = Convert_Decimal_Binary(numero)
+    print(f'{binario} es el numero en Binario de {numero}')
+
+if __name__ == '__main__':
+    run()
