5c84b0afcab4484210ed8a70e30bb18a0d1bc889
Jose A. De la hoz <josehoz.ing@gmail.com>
2022-07-14 15:52:08 -0500

    Solucion reto filtrando datos

---
+++ Curso_python_intermedio/filtering_data_challenge.py
@@ -0,0 +1,100 @@
+
+from concurrent.futures.thread import _worker
+from subprocess import list2cmdline
+
+
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run():
+    #With high order functions (filter)
+    all_python_devs_name = list(filter(lambda worker: worker["language"] == "python", DATA))
+
+    for worker in all_python_devs_name:
+        print(worker)
+
+    #With high order functions (filter)
+    all_workers_platzi = list(filter(lambda worker: worker["organization"] == "Platzi", DATA))
+    for worker in all_workers_platzi:
+        print(worker)
+
+    adults = [worker["name"] for worker in DATA if worker["age"] > 18]
+    for worker in adults:
+        print(worker)
+    
+    old_people = [worker["name"] for worker in DATA if worker["age"] > 70]
+    for worker in old_people:
+        print(worker)
+
+if __name__ == '__main__':
+    run()
